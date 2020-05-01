e7a0bfcc040b3781a000d2f3795aafab6ab6c65b
Trevor Low <tlow94@uw.edu>
2020-05-01 01:10:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,10 @@ class Student
     @grade = grade
     @id = id
   end
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS student(id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
