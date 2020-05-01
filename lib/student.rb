73ec1fc95b44847a10b02cf575ab7797bdf6fe50
Trevor Low <tlow94@uw.edu>
2020-05-01 01:13:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,5 +20,6 @@ class Student
     INSERT INTO students(name, grade) VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
   end
 end
