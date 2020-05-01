487934ae957cf1c38da55b1f8dc2dec867e55e00
Trevor Low <tlow94@uw.edu>
2020-05-01 01:07:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,9 @@
 class Student
-  attr_accessor :id
-  def initialize(id)
+  attr_accessor :name, :grade
+  attr_reader :id
+  def initialize (name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
   end
 end
